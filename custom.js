20b1627e1e1a26ef9c990e00656ad346915d5b40
Shonin <hello@dire.io>
2016-06-29 20:04:01 +0000

    make hover js

--- custom.js
+++ custom.js
@@ -1,3 +1,20 @@
+/* global $ */
+
 $(document).ready(function() {
-//   $("body").html("hello world")
+
+    $(".project-link-past").on("mouseover", function() {
+      $(this).css('background-image', 'url("brent_haas_files/tab_hover.png")');
+      $(this).find('p').css('color', '#1B191A');
+      $(this).animate({top : '250px'});
+     
+    });
+    
+    
+    
+    $(".project-link-past").on("mouseout", function() {
+        $(this).css('background-image', '');
+        $(this).find('p').css('color', '');
+     
+    });
+    
 })
