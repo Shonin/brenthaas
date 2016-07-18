33517b8d11465df7224bc0d9ff4a1e51fd2e70b2
Shonin <hello@dire.io>
2016-07-18 16:09:34 +0000

    finish animation

--- custom.js
+++ custom.js
@@ -2,44 +2,38 @@
 
 $(document).ready(function() {
     
-    $("#card1").flip({axis: 'x', speed: '600'});
-    $("#card2").flip({axis: 'x', speed: '600'});
-    $("#card3").flip({axis: 'x', speed: '600'});
-    $("#card4").flip({axis: 'x', speed: '600'});
-    
-    $(".project-link-past > #card1").on("mouseover", function() {
-        $("#card1").flip(true);
-    });
-    
-    $(".project-link-past > #card2").on("mouseover", function() {
-        $("#card2").flip(true);
-    });
-    
-    $(".project-link-past > #card3").on("mouseover", function() {
-        $("#card3").flip(true);
-    });
-    
-    $(".project-link-past > #card4").on("mouseover", function() {
-        $("#card4").flip(true);
-    });
-
-
-
-
     $(".project-link-past").on("mouseover", function() {
       $(this).css('background-image', 'url("brent_haas_files/tab_hover.png")');
       $(this).find('p').css('color', '#1B191A');
-    //   $(this).animate({top : '250px'});
     });
     
     $(".project-link-past").on("mouseout", function() {
         $(this).css('background-image', '');
         $(this).find('p').css('color', '');
-        $("#card1").flip(false);
-        $("#card2").flip(false);
-        $("#card3").flip(false);
-        $("#card4").flip(false);
     });
     
+    
+    
+    function flipTitle(card) {
+        $(card).flip({axis: 'x', speed: '600'})
+        $(".project-link-past >" + card).on("mouseover", function() {
+            $(card).flip(true)
+        })
+        $(".project-link-past").on("mouseout", function() {
+            $(card).flip(false);
+        });
+    }
+    
+    flipTitle("#card1");
+    flipTitle("#card2");
+    flipTitle("#card3");
+    flipTitle("#card4");
+    flipTitle("#card5");
+    flipTitle("#card6");
+    flipTitle("#card7");
+    flipTitle("#card8");
+    flipTitle("#card9");
+    flipTitle("#card10");
+    flipTitle("#card11");
     
 })
