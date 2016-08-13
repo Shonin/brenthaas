2f37f1c111a440fdd3776354829a70362399283d
Elliot Charney <elliot.charney@ithaka.org>
2016-08-13 16:25:36 -0400

    add color

--- custom.js
+++ custom.js
@@ -18,10 +18,15 @@ $(document).ready(function() {
         $(card).flip({axis: 'x', speed: '600'})
         $(".tab >" + card).on("mouseover", function() {
             $(card).flip(true)
-        })
+        });
         $(".tab").on("mouseout", function() {
             $(card).flip(false);
         });
+
+        $(".tab >" + card).on("click", function() {
+            $(card).flip(true)
+        });
+
     }
     
     flipTitle("#card1");
@@ -35,6 +40,30 @@ $(document).ready(function() {
     flipTitle("#card9");
     flipTitle("#card10");
     flipTitle("#card11");
-    flipTitle("#card-current")
+    
+
+
+    $(".tab0").on("mouseover", function() {
+      $(this).css('background-image', 'url("brent_haas_files/tab-red-hover.png")');
+      $(this).find('p').css('color', '#1B191A');
+    });
+
+    $(".tab0").on("mouseout", function() {
+        $(this).css('background-image', '');
+        $(this).find('p').css('color', '');
+    });
+
+    function flipTitle0(card) {
+        $(card).flip({axis: 'x', speed: '600'})
+        $(".tab0 >" + card).on("mouseover", function() {
+            $(card).flip(true)
+        })
+        $(".tab0").on("mouseout", function() {
+            $(card).flip(false);
+        });
+    }
+
+    flipTitle0("#card-current");
+
     
 })
